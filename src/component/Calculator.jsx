ea2cfb74fd320cbec34fad4cbfcbe1377f797965
Nizamuddin Ahmadzai <78977308+Nizamuddin4493@users.noreply.github.com>
2022-08-02 23:28:38 +0430

    setup the component directory

---
+++ src/component/Calculator.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = 0;
+  }
+
+  render() {
+    return (
+      <div>Calculator</div>
+    );
+  }
+}
+
+export default Calculator;
